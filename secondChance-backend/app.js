1b039c0bcd86132ddfe989bf71534a7c5ab02187
Muhamad Fajar <70709766+mfajar2123@users.noreply.github.com>
2024-10-09 15:52:31 +0700

    Update app.js

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -4,21 +4,16 @@ const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
 const path = require('path');
-
 const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
-
 
 const app = express();
-app.use("*",cors());
+app.use("*", cors());
 const port = 3060;
 
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
-})
-    .catch((e) => console.error('Failed to connect to DB', e));
-
+}).catch((e) => console.error('Failed to connect to DB', e));
 
 app.use(express.json());
 
@@ -43,9 +38,9 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal Server Error');
 });
 
-app.get("/",(req,res)=>{
-    res.send("Inside the server")
-})
+app.get("/", (req, res) => {
+    res.send("Inside the server");
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
